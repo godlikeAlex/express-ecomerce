c7c82de6a3aee8d88bf490842e3a9b0582ef7bca
Aleksandr Yurkovsky <godlikedesigner@gmail.com>
2019-10-31 23:55:00 +0500

    added connection to mongodb

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app     = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+
+// Database Connection
+mongoose.connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+    .then(() => console.log('Database success connected.'))
+    .catch(() => console.error('Error to connect to database.'));
+
+// Routes
 app.get('/', (req, res) => {
     res.send('hello from node');
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3030;
 
-app.listen(port, () => {
+app.listen(3030, () => {
     console.log('Server Started!');
 });
