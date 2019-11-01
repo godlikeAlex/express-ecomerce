3ee73b7ffbe59a4637d42d6975257ea09e5ac75e
Aleksandr Yurkovsky <godlikedesigner@gmail.com>
2019-11-01 23:09:56 +0500

    added controller User for route user

---
+++ controllers/user.js
@@ -0,0 +1,3 @@
+exports.create = (req, res) => {
+    res.json({hello: 'Aleksandr'});
+};
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,7 @@
 const express   = require('express');
 const router    = express.Router();
+const {create}        = require('../controllers/user');
 
-router.get('/', (req, res) => {
-    res.send('Hello from nodes');
-});
+router.get('/', create);
 
 module.exports = router;
