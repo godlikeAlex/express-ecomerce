45943e40ac3e1a88f92c61ffe8f114fbb5586f52
Aleksandr Yurkovsky <godlikedesigner@gmail.com>
2019-11-04 23:52:37 +0500

    added sign in route and controller and method auth

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,9 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const expressjwt = require('express-jwt');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.signup = (req, res) => {
-    console.log(req.body);
     const createdUser = new User(req.body);
     createdUser.save()
         .then(user => {
@@ -10,5 +11,27 @@ exports.signup = (req, res) => {
             user.hashed_password = undefined;
             res.status(200).json(user);
         })
-        .catch(error => res.status(400).json({err: errorHandler(error)}));
+        .catch(error => res.status(400).json({error: errorHandler(error)}));
+};
+
+exports.signin = (req, res) => {
+    const {email, password} = req.body;
+
+    User.findOne({email}, (err, user) => {
+        if(err && !user) {
+            return res.status(400).json({error: `User with email - ${email} not found.`});
+        }
+
+        if(!user.authenticate(password)) {
+            return res.status(401).json({
+                error: 'Email and password dont match'
+            })
+        }
+
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+        res.cookie('t', token, {expire: new Date() + 9999});
+
+        const {_id, name, email, role} = user;
+        return res.status(200).json({token, user: {_id, name, email, role}});
+    })
 };
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express   = require('express');
 const router    = express.Router();
-const { signup }        = require('../controllers/user');
+const { signup, signin }        = require('../controllers/user');
 const {userSignUpValidator} = require('../validator');
 
 router.post('/signup', userSignUpValidator, signup);
+router.post('/signin', userSignUpValidator, signin);
 
 module.exports = router;
