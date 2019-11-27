326f03ef17dbad774e1de7b7285fa8242d58a846
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2019-11-28 00:26:24 +0500

    added new moder proudct

---
+++ models/product.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const {ObjectId} = mongoose.Schema;
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        maxLength: 32,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 2000
+    },
+    price: {
+        type: Number,
+        required: true,
+        maxLength: 2000
+    },
+    category: {
+        type: ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    quantity: {
+        type: Number
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    shipping: {
+        required: false,
+        type: Boolean
+    }
+
+}, {timestamps: true});
+
+module.exports = mongoose.model('Product', ProductSchema);
