5b0e63e68c3903571905993f399afd3b576d9ae9
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2019-12-03 22:25:32 +0500

    added new route and controller for update product

--- controllers/product.js
+++ controllers/product.js
@@ -46,6 +46,35 @@ exports.create = (req, res) => {
     });
 };
 
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if(err) return res.status(400).json({err: "Something went wrong"});
+
+        const { name, description, price, category, shipping, quantity, photo } = fields;
+
+        if(!name || !description || !price || !category || !shipping || !quantity) {
+            return res.status(400).json({err: "Missing required fields."});
+        }
+
+        let createdProduct = req.product;
+        createdProduct = _.extend(createdProduct, fields);
+
+        if(files.photo) {
+            if(files.photo.size > 300000) {
+                return  res.status(400).json({err: "Image should be less then 1mb."});
+            }
+            createdProduct.photo.data = fs.readFileSync(files.photo.path);
+            console.log(files.photo.path);
+            createdProduct.photo.contentType = files.photo.type;
+        }
+        createdProduct.save()
+            .then(data => { return res.status(200).json(createdProduct) })
+            .catch(err => { return res.json(400).json({err: errorHandler(err)}) })
+    });
+};
+
 exports.remove = (req, res) => {
     const product = req.product;
     product.remove()
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 const express   = require('express');
 const router    = express.Router();
-const { create, productById, read, remove }        = require('../controllers/product');
+const { create, productById, read, remove, update }        = require('../controllers/product');
 const { requireSignIn, isAuth, isAdmin }   = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 router.get('/product/:productId', read);
 router.post('/product/create/:userId', requireSignIn, isAuth, isAdmin, create);
+router.put('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, update);
 router.delete('/product/:productId/:userId', requireSignIn, isAuth, isAdmin, remove);
 
 router.param('userId', userById);
