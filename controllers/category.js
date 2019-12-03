2743b232618fa6574c39a2eca5af5221a5404c7f
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2019-12-03 23:37:54 +0500

    added new midleware categoryById and new route for read category

--- controllers/category.js
+++ controllers/category.js
@@ -1,9 +1,23 @@
 const Category = require('../models/category');
 const errorHandler = require('../helpers/dbErrorHandler');
 
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if(err || !category) {
+            return res.status(200).json({'err': 'Category not found'});
+        }
+        req.category = category;
+        next();
+    });
+};
+
 exports.createCategory = (req, res) => {
     const category = new Category(req.body);
     category.save()
         .then(data => res.status(200).json({data}))
         .catch(err => res.status(400).json({"err": errorHandler(err)}));
+};
+
+exports.read = (req, res) => {
+    return res.json(req.category);
 };
--- routes/category.js
+++ routes/category.js
@@ -1,10 +1,12 @@
 const express   = require('express');
 const router    = express.Router();
-const { createCategory }        = require('../controllers/category');
+const { createCategory, read, categoryById }        = require('../controllers/category');
 const { requireSignIn, isAdmin }   = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
+router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignIn, isAdmin, createCategory);
 router.param('userId', userById);
+router.param('categoryId', categoryById);
 
 module.exports = router;
